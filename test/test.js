f955a91d4629165b7b2fd864177c0b202a76e184
Carlos Castro <alu0100819847@ull.edu.es>
2017-05-16 19:44:06 +0100

    Test añadidos

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var Igual = require('../lib/Igual.js');
+
+describe("Función igual", function(){
+  it("La función igual debe devolver los elementos comunes entre sec1 y sec2", function() {
+    var sec1 = [1, 2, 3];
+    var sec2 = [2, 3, 4];
+    var result = Igual(sec1, sec2)
+    result.should.be.equal('2, 3');
+  }
+
+})
